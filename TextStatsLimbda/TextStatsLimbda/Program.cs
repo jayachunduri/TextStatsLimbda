c2b50fb3d71154d27d78827cabf5174aeed7994d
jaya chunduri <jaya.chunduri@gmail.com>
2014-09-09 12:39:59 -0600

    TextStatsModified

--- TextStatsLimbda/TextStatsLimbda/Program.cs
+++ TextStatsLimbda/TextStatsLimbda/Program.cs
@@ -21,11 +21,17 @@ namespace TextStatsLimbda
 
             Console.WriteLine("Number of Words: " +input.Split(' ').Count());
 
-            Console.WriteLine("Number of vowels: " +input.ToLower().Count(x => (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')));
+            //Console.WriteLine("Number of vowels: " +input.ToLower().Count(x => (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u')));
+            //below is the easy way to do it.
+            Console.WriteLine("Number of vowels: " + input.ToLower().Count(x => "aeiou".Contains(x)));
+            
+            //Console.WriteLine("Number of consonants: " +input.ToLower().Count(x => !(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == ' ' || x == '?' || x == '!' || x == '.' || x == ',')));
+            //below is the easy way to do it.
+            Console.WriteLine("Number of consonants: " + input.ToLower().Count(x => "bcdfghjklmnpqrstvwxyz".Contains(x)));
 
-            Console.WriteLine("Number of consonants: " +input.ToLower().Count(x => !(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == ' ' || x == '?' || x == '!' || x == '.' || x == ',')));
-
-            Console.WriteLine("Number of special characters: " +input.Count(x => (x==' ' || x == '?' || x == '!' || x == '.' || x == ',')));
+            //Console.WriteLine("Number of special characters: " +input.Count(x => (x==' ' || x == '?' || x == '!' || x == '.' || x == ',')));
+            //below is the easy way to do it.
+            Console.WriteLine("Number of special characters: " + input.Count(x => "!@#$%^&*()_+ ".Contains(x)));
 
             Console.WriteLine("Longest Word: " +input.Split(' ').OrderByDescending(x => x.Length).First());
 
